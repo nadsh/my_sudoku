568d18ad6acc0db82951d344db7b9919d0013d47
Nadav <snadsh@gmail.com>
2024-06-28 21:05:55 +0300

    removed dead code

--- sudoku_board.py
+++ sudoku_board.py
@@ -204,17 +204,6 @@ class MainWindow(QtWidgets.QMainWindow):
             self.status_label.setText('Sudoku is not solved yet.')
 
 def main():
-    sudoku_grid = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]
     sudoku_grid = sudoku_generator.get_new_sudoku_as_list()
 
     app = QtWidgets.QApplication(sys.argv)
